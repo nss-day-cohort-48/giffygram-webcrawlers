005a42ec5edde71afda1055b3eec86296942521b
Ben Kimball <bkimball97@gmail.com>
2021-05-20 10:44:09 -0500

    commit to pull latest changes

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,18 +1,18 @@
 import { NavBar } from "./nav/NavBar.js"
-=======
 import { PostEntry } from "./feed/PostEntry.js"
 import { PostList } from "./feed/PostList.js"
+import { MessageForm } from "./message/MessageForm.js"
+
 
 export const GiffyGram = () => {
 
     // Show main main UI
     return `<h1>Giffygram</h1>
         ${NavBar()}
-
-    `
-}
+        ${MessageForm()}
     <div class="postEntry">
     <div class="miniMode" id="miniMode">Have a gif to post?
     </div></div>
-    ${ PostList() }`
+    ${ PostList() }` 
+}
 
