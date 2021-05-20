3ad85ebcb9f34a18fca154db55473bc9982bcaac
Devin Garcia <devinmgarcia@gmail.com>
2021-05-20 10:24:22 -0500

    save

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -5,11 +5,9 @@ import { PostList } from "./feed/PostList.js"
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>
-
+    return `
     <div class="postEntry">
     <div class="miniMode" id="miniMode">Have a gif to post?
     </div></div>
     ${ PostList() }`
-
-
+}
