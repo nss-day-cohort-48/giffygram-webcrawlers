ace8a5cf09aeca36a72eedab5c92efc5793c1517
Ben Kimball <bkimball97@gmail.com>
2021-05-25 10:46:46 -0500

    commiting to pull new main

--- src/scripts/message/MessageList.js
+++ src/scripts/message/MessageList.js
@@ -1,11 +1,13 @@
 import { getMessages, getUsers } from "../data/provider.js"
 
+
 export const MessageList = () => {
     const messages = getMessages()
     const users = getUsers()
     const currentUser = parseInt(localStorage.getItem("gg_user")) //
 
-    const html = `<div class="messageList">
+    const html = `
+    <div class="messageList">
         ${messages.map(message => {
             const sender = users.find(user => message.userId === user.id)
             if(currentUser === message.recipientId) // checks to make sure that the users id that is stored in local storage is strictly equal to the recipientId on the message object
@@ -15,8 +17,6 @@ export const MessageList = () => {
                     </div>`
             }).join("")
         }
-
-        
     </div>`
 
     return html
